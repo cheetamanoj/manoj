3c60ab53039b8d7b9017548d19dffddb47a61444
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-01-21 15:28:21 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,N;
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+N=a<b;
+N<100000?printf("positive"):printf("negative");
+}
