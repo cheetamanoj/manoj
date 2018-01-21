03d026cf7a6042d6592b0b613821ea9dfb7fa4fa
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-01-21 15:09:43 +0530

    Create mech.c

---
+++ mech.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include(conio.h>
+voidmain()
+{
+int a,b,N;
+clrscr();
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+N=a<b;
+N<100000?printf("positive"):printf("negative"):
+getch();
+}
+
+
