67200f5e8bee8f1849fc1fc586ab9627e99d8fa7
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-01-30 16:45:55 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+        int i,n;
+        printf("\n Please Give The Value of N:");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %d = %d",i,5*i);
+        }
+}
+
