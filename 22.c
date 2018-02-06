73d4a76ee67573e8dc87ac07b18d57245c227a64
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-02-06 16:22:15 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+   void main()
+   {
+       int a,b,c,d,e,f;
+       printf("enter value a");
+       scanf("%d",&a);
+       printf("\n enter value b");
+       scanf("%d",&b);
+       printf("\n enter value of c");
+       scanf("%d",&c);
+       d=(a*b);
+       e=(d*c);
+       f=(d+e);
+       printf("value of f=%d",f);
+   }
