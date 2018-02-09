85037d354670e42cb8aa5942bf41f46d627d30d5
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-02-09 16:24:43 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i, n,max,a[10];
+    printf("enter the range");
+    scanf("%d",&n);
+    printf("enter array values");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+    {
+    max=a[i];
+        printf("%d",max);
+    }
+    
