cd95d65fc0325f9abaaf7f8de94acb3a6894c2df
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-02 14:15:22 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() 
+{
+int a;
+printf("Enter a number:");
+scanf("%d",&a);
+if(isdigit(a))
+{
+print("It is a num:%d");
+}
+else
+{
+printf("It is not a Number);
+}
+return 0;
+}
+
