5f653f35fca976021f3645cedac34916cd58f2b5
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-02 14:27:00 +0530

    Create 30a.c

---
+++ 30a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int a,b,c,d,e, min,hr;
+printf("Enter the starting time");
+scanf("%d",&b);
+scanf("%d",&a);
+printf("Enter the current time");
+scanf("%d",&d);
+scanf("%d",&c);
+min=b-c;
+hr=e-d;
+printf("The diff b/w time periods is....%d:%d",hr,min);
+}
