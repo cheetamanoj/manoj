ec6edd16a2f295107c41cc8b2b245d61cd9419a3
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-13 16:27:25 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(a!=0)
+{
+a/=10;
+++count;
+}
+printf("the count is %d",count);
+}
