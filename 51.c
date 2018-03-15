618ef5e9f5193398d8af640d1c41c95ab0c2d32c
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-15 16:45:08 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i=0,a=0,i[100],j;
+	printf("enter the number ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		i=n%10;
+		n=n/10;
+		a[i]=x;
+		i++;
+	}
+	
+	for(j=i-1;j>=0;j--)
+	printf("%d ",a[j]);
+}
+
