6dbea02af92f6bd89b7c37faf64624ba3832476e
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-20 16:47:00 +0530

    Create 70a.c

---
+++ 70a.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+
+{
+
+int n,a=0,x=1,i;
+
+scanf("%d",&n)
+
+while(n!=1)
+
+{
+
+n=n/2;
+
+a++;
+
+}
+
+for(i=0;i<c+1;i++);
+
+x=x*2;
+
+printf("%d",x);
+return 0;
+}
