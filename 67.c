525c864fbb9d3d5641c717f3a11d57069eaf5c69
cheetamanoj <35652729+cheetamanoj@users.noreply.github.com>
2018-03-20 16:35:24 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    inta,n;
+    scanf("%d",&n);
+    
+    a=n+1;
+    while(a!=0)
+    {
+        if(a%10==0)
+        {
+            printf("%d",a);
+            break;
+        }
+        a++;
+    }
+  
+}
